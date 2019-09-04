9e85e01c3a1882fab8d98617bbac3fe4b7e69b30
Bernardo Vieira <bernardo5@live.com.pt>
2019-09-04 22:26:23 +0100

    add social media links

--- src/Pages/Main/index.jsx
+++ src/Pages/Main/index.jsx
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
 import profile from '../../Assets/profile.jpg';
 import twitterLogo from '../../Assets/twitter.svg';
+import linkedinLogo from '../../Assets/linkedin.svg';
+import githubLogo from '../../Assets/github.svg';
+import envelope from '../../Assets/envelope.svg';
 import './index.css';
 
 
 class Main extends Component {
+    state = {
+        hello: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in sollicitudin arcu.',
+        me: 'Vivamus elementum lectus pharetra ligula egestas, ut vestibulum magna ullamcorper. Quisque nec nisi in nulla molestie interdum. Vestibulum id semper odio. Vestibulum quam mauris, sagittis id hendrerit ut, aliquam lacinia orci. Fusce laoreet blandit erat, vel congue purus sagittis nec.',
+    }
 
     /**
      * @ignore
      */
     render() {
+        const { hello, me } = this.state;
         return (
             <div className="main-content">
                 <div className="grey-box" />
                 <div style={{ float: 'right' }}>
-                    <img className="picture" src={profile} alt="profile" />
+                    <img className="picture" src={profile} alt="obernardovieira profile" />
                 </div>
                 <div className="text-content">
-                    <h1 className="title is-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in sollicitudin arcu.</h1>
-                    <p style={{ padding: '50px' }}>Vivamus elementum lectus pharetra ligula egestas, ut vestibulum magna ullamcorper. Quisque nec nisi in nulla molestie interdum. Vestibulum id semper odio. Vestibulum quam mauris, sagittis id hendrerit ut, aliquam lacinia orci. Fusce laoreet blandit erat, vel congue purus sagittis nec.</p>
+                    <h1 className="title is-1">{hello}</h1>
+                    <p style={{ padding: '50px' }}>{me}</p>
                     <div className="social-media">
-                        <a href="www.twitter.com">
+                        <a href="https://twitter.com/obernardovieira">
                             <img className="social-logo" src={twitterLogo} alt="twitter logo" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/obernardovieira/">
+                            <img className="social-logo" src={linkedinLogo} alt="linkedin logo" />
+                        </a>
+                        <a href="https://github.com/obernardovieira">
+                            <img className="social-logo" src={githubLogo} alt="github logo" />
+                        </a>
+                        <a href="mailto:bernardo@techhq.io">
+                            <img className="social-logo" src={envelope} alt="envelope logo" />
                         </a>
                     </div>
                 </div>
