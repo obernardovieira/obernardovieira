90648383f4283345ace880a8485a4ab9e483cba7
Bernardo Vieira <bernardo5@live.com.pt>
2019-09-14 16:20:10 +0100

    finish fullhq screen and draft hd screen

--- src/Pages/Main/index.jsx
+++ src/Pages/Main/index.jsx
@@ -6,6 +6,7 @@ import twitterLogo from '../../Assets/twitter.svg';
 import linkedinLogo from '../../Assets/linkedin.svg';
 import githubLogo from '../../Assets/github.svg';
 import envelope from '../../Assets/envelope.svg';
+import layout from '../../Assets/layout.svg';
 import messages from './messages.json';
 import './index.css';
 
@@ -41,7 +42,25 @@ class Main extends Component {
                         </a>
                     </div>
                 </div>
-                <div className="also">{also}</div>
+                <div style={{ textAlign: 'center' }}>
+                    <div className="working-on-title"><h1 className="title is-1">Have a look at what I've been working on</h1></div>
+                </div>
+                <div className="also">
+                    <div>
+                        <img className="article-img" src={layout} alt="layout" />
+                        <div className="article-info">
+                            <h4 className="title is-4">Hackathons</h4>
+                            <span>I've been joinning and winning hackathons for a living. See the full list. You might have been in one already.</span>
+                        </div>
+                    </div>
+                    <div>
+                        <img className="article-img" src={layout} alt="layout" />
+                        <div className="article-info">
+                            <h4 className="title is-4">Articles</h4>
+                            <span>Talk is cheap. I like to write, although tomorrow it might be deprecated, but still, I write. I think you will like it.</span>
+                        </div>
+                    </div>
+                </div>
                 <ReactTooltip />
             </div>
         );
