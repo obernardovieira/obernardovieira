61e7b8d369ff770e93fe4849272794a413c47ec3
Bernardo Vieira <bernardo5@live.com.pt>
2019-10-04 22:38:45 +0100

    finish main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Main from './Pages/Main';
 
 export default function App() {
     return (
         <div>
-            <Router>
-                <div>
-                    <Route path="/" exact component={Main} />
-                </div>
-            </Router>
+            <Main />
         </div>
     );
 }
--- src/Pages/Main/index.jsx
+++ src/Pages/Main/index.jsx
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
-import ReactTooltip from 'react-tooltip'
 
 import profile from '../../Assets/profile.jpg';
 import twitterLogo from '../../Assets/twitter.svg';
 import linkedinLogo from '../../Assets/linkedin.svg';
 import githubLogo from '../../Assets/github.svg';
 import envelope from '../../Assets/envelope.svg';
-import layout from '../../Assets/layout.svg';
+import writer from '../../Assets/writer.svg';
+import hacker from '../../Assets/hacker.svg';
 import messages from './messages.json';
 import './index.css';
 
 
 class Main extends Component {
 
+    sendHackathons = (event) => {
+        window.location.href = 'https://gist.github.com/obernardovieira/f4ec9b75736a98be5f6198f5ae40b897';
+        event.preventDefault();
+    }
+
+    sendArticles = (event) => {
+        window.location.href = 'https://www.linkedin.com/in/obernardovieira/detail/recent-activity/posts/';
+        event.preventDefault();
+    }
+
     /**
      * @ignore
      */
     render() {
-        const { hello, me, also } = messages;
+        const { hello, me } = messages;
         return (
             <div className="main-content">
                 <div className="grey-box" />
@@ -29,16 +39,16 @@ class Main extends Component {
                     <p className="secondary-message" >{me}</p>
                     <div className="social-media">
                         <a href="https://twitter.com/obernardovieira">
-                            <img data-tip="Find me on Twitter" className="social-logo" src={twitterLogo} alt="twitter logo" />
+                            <img className="social-logo" src={twitterLogo} alt="twitter logo" />
                         </a>
                         <a href="https://www.linkedin.com/in/obernardovieira/">
-                            <img data-tip="Find me on LinkedIn" className="social-logo" src={linkedinLogo} alt="linkedin logo" />
+                            <img className="social-logo" src={linkedinLogo} alt="linkedin logo" />
                         </a>
                         <a href="https://github.com/obernardovieira">
-                            <img data-tip="Find me on GitHub" className="social-logo" src={githubLogo} alt="github logo" />
+                            <img className="social-logo" src={githubLogo} alt="github logo" />
                         </a>
                         <a href="mailto:bernardo@techhq.io">
-                            <img data-tip="Send me an Email" className="social-logo" src={envelope} alt="envelope logo" />
+                            <img className="social-logo" src={envelope} alt="envelope logo" />
                         </a>
                     </div>
                 </div>
@@ -46,22 +56,21 @@ class Main extends Component {
                     <div className="working-on-title"><h1 className="title is-1 my-title">Have a look at what I've been working on</h1></div>
                 </div>
                 <div className="also">
-                    <div>
-                        <img className="article-img" src={layout} alt="layout" />
+                    <div onClick={this.sendHackathons}>
+                        <img className="article-img" src={hacker} alt="layout" />
                         <div className="article-info">
                             <h4 className="title is-4">Hackathons</h4>
                             <span>I've been joinning and winning hackathons for a living. See the full list. You might have been in one already.</span>
                         </div>
                     </div>
-                    <div>
-                        <img className="article-img" src={layout} alt="layout" />
+                    <div onClick={this.sendArticles}>
+                        <img className="article-img" src={writer} alt="layout" />
                         <div className="article-info">
                             <h4 className="title is-4">Articles</h4>
                             <span>Talk is cheap. I like to write, although tomorrow it might be deprecated, but still, I write. I think you will like it.</span>
                         </div>
                     </div>
                 </div>
-                <ReactTooltip />
             </div>
         );
     }
